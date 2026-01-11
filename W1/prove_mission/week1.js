596cf585e6804cfa9a8dc3b5639c379fd4aafce8
Adam Egbert <adamegbert@icloud.com>
2026-01-10 17:40:35 -0700

    finished the ponder assignment

---
+++ W1/prove_mission/week1.js
@@ -0,0 +1,27 @@
+
+const PI = 3.14;
+let radius = 3;
+                  
+
+// Calculate the area
+area = radius * radius * PI;
+
+// 3. OUTPUT (Print the result to the hidden console)
+console.log("The area is:");
+console.log(area);
+
+
+const one = 1;
+const two = '2';
+                    
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
